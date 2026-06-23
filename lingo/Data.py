4712a2c14513611528dba68637dba5128de6ea80
kolpakchi11 <kolpakciandrij@gmail.com>
2026-06-23 18:24:23 +0200

    mini- update

--- lingo/Data.py
+++ lingo/Data.py
@@ -2,7 +2,7 @@ WOORDENLIJST = [
     "mysql", "brood", "chaos", "draak", "java",
     "fatbike", "groen", "hamer", "Japan", "kamer",
     "lemon", "mango", "nacht", "rekenen", "robots",
-    "ai", "google", "github", "water", "google",
+    "ai", "google", "github", "water", "car",
     "programming", "cirkel", "stroom", "kapot", "fakkel",
     "debug", "design", "website", "wifi", "kabel",
 ]
